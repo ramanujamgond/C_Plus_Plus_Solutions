f44dc9e880416ef677ba257697bba6dd139b554b
Ramanujam Gond <ramanujamgond@gmail.com>
2018-05-24 10:57:02 +0530

    Modified indentations of comments

--- Chapter 6 Exercise_files/exercise_3.cpp
+++ Chapter 6 Exercise_files/exercise_3.cpp
@@ -10,17 +10,17 @@ class times {
 		int minutes;
 		int seconds;
 	public:
-		times():hours(0), minutes(0), seconds(0)								//no-argument constructor
+		times():hours(0), minutes(0), seconds(0) //no-argument constructor
 			{	}
 		times(int hrs, int min, int sec):hours(hrs), minutes(min), seconds(sec) //3-argument constructor
 			{	}
-		void dispTime() const {													//display time in hh:mm:ss format
+		void dispTime() const { //display time in hh:mm:ss format
 			cout <<hours <<":" <<minutes <<":" <<seconds;
 		}
-		void addTime(times, times);												//add times arguments #declaration
+		void addTime(times, times); //add times arguments #declaration
 };
 
-void times::addTime(times t1, times t2) {										//add times arguments #defination
+void times::addTime(times t1, times t2) { //add times arguments #defination
 	seconds = t1.seconds + t2.seconds;
 	if(seconds > 59) {
 		seconds -= 60;
@@ -36,12 +36,12 @@ void times::addTime(times t1, times t2) {										//add times arguments #defina
 }
 
 int main() {
-	const times t1(10, 30, 43);													//create and initialize times objects
-	const times t2(2, 26, 53);													//create and initialize times objects
-	times t3;																	//create times object
-	t3.addTime(t1, t2);															//calling addTime to add two times object
+	const times t1(10, 30, 43); //create and initialize times objects
+	const times t2(2, 26, 53); //create and initialize times objects
+	times t3; //create times object
+	t3.addTime(t1, t2); //calling addTime to add two times object
 	cout <<"Total time in t3 is: ";
-	t3.dispTime();																//calling dispTime to display time
+	t3.dispTime(); //calling dispTime to display time
 	cout <<endl;
 	return 0;
 }
